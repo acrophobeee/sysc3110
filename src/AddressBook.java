4a8c415a2475eb3da54c6591ab2352b4af154931
xinyuchen <xinyuchen@mc6045-30.labs.sce.carleton.ca>
2018-09-14 10:11:00 -0400

    change

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -2,11 +2,16 @@ import java.util.ArrayList;
 
 public class AddressBook {
     
-	ArrayList<BuddyInfo> a;
+	ArrayList<BuddyInfo> a = new ArrayList<BuddyInfo>();
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("Address book");     
+       System.out.println("Address book");    
+       AddressBook ad= new AddressBook();
+       BuddyInfo q= new BuddyInfo("xinyuchen","southgate","101031031");
+       ad.addBuddy(q);
+       ad.removeBuddy(0);
+         
 	}
     public void addBuddy(BuddyInfo aa) {
     	if(aa != null) {
